9b3e1e952da7bf296c4157a7ff997fc643380646
keduard2006 <131702439+keduard2006@users.noreply.github.com>
2024-06-11 19:17:28 -0500

    Update Sistema de Gestion de Recursos Humanos.cpp

--- Sistema de Gestion de Recursos Humanos.cpp
+++ Sistema de Gestion de Recursos Humanos.cpp
@@ -222,23 +222,3 @@ public:
         return getSalario() + comisionProyectos;
     }
 };
-
-int main() {
-    // Creando empleados de diferentes tipos
-    Gerente PedroPaz("Pedro Paz", 2500, 2022, 3, 1, 400);
-    Gerente LiamSol("Liam Sol", 2800, 2021, 7, 15, 400);
-    Desarrollador MariaVelez("Maria Velez", 2000, 2023, 5, 10, 10, 35);
-    Diseñador SimeoVill("Simeo Vill", 1800, 2024, 2, 15, 200);
-
-    // Demostrando el polimorfismo para calcular salarios
-    cout << "Nombre: " << PedroPaz.getNombre() << endl;
-    cout << "Salario: " << PedroPaz.calcularSalario() << endl;
-    cout << "Nombre: " << LiamSol.getNombre() << endl;
-    cout << "Salario: " << LiamSol.calcularSalario() << endl;
-    cout << "Nombre: " << MariaVelez.getNombre() << endl;
-    cout << "Salario: " << MariaVelez.calcularSalario() << endl;
-    cout << "Nombre: " << SimeoVill.getNombre() << endl;
-    cout << "Salario: " << SimeoVill.calcularSalario() << endl;
-
-    return 0;
-}
