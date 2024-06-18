758d6d1a60586029ee8b94cd14b495a24a33ab34
keduard2006 <131702439+keduard2006@users.noreply.github.com>
2024-06-18 16:16:51 -0500

    Update Sistema de Gestion de Recursos Humanos.cpp

--- Sistema de Gestion de Recursos Humanos.cpp
+++ Sistema de Gestion de Recursos Humanos.cpp
@@ -1,144 +1,4 @@
 #include <iostream>
-
-// Clase base Empleado
-class Empleado {
-protected:
-    string nombre;
-    double salarioBase;
-    int fechaContratacion;
-public:
-    // Constructor
-    Empleado(double salario) : salarioBase(salario) {}
-    // Método virtual para calcular el salario
-    virtual double calcularSalario();
-};
-// Clase derivada Gerente
-class Gerente : public Empleado {
-public:
-    // Constructor
-    Gerente(double salario) : Empleado(salario) {}
-    double calcularSalario() const override {
-        return salarioBase;
-    }
-};
-// Clase derivada Desarrollador
-class Desarrollador : public Empleado {
-public:
-    // Constructor
-    Desarrollador(double salario) : Empleado(salario) {}
-    double calcularSalario() const override {
-        return salarioBase / 2.0;
-    }
--------------------------------
-#include <iostream>
-
-using namespace std;
-
-class Empleado {
-private:
-  string nombre;
-  float salario;
-  int fechaContratacion;
-
-public:
-  Empleado(string nombre, float salario, int fechaContratacion) {
-    this->nombre = nombre;
-    this->salario = salario;
-    this->fechaContratacion = fechaContratacion;
-  }
-
-  virtual float calcularSalario(); // Método virtual
-
-  string getNombre() const { 
-      return nombre; 
-  }
-  void setNombre(const string& nombre) { 
-      this->nombre = nombre; 
-  }
-
-  float getSalario() const { 
-      return salario; 
-  }
-  void setSalario(float salario) { 
-      this->salario = salario; 
-  }
-
-  fecha getFechaContratacion() const { 
-      return fechaContratacion; 
-  }
-  void setFechaContratacion(const fecha& fechaContratacion) { 
-      this->fechaContratacion = fechaContratacion; 
-  }
-};
-
-class Gerente : public Empleado {
-private:
-  float bono;
-
-public:
-  Gerente(string nombre, float salario, fecha fechaContratacion, float bono) : Empleado(nombre, salario, fechaContratacion) {
-    this->bono = bono;
-  }
-
-  float calcularSalario() { // Método virtual
-    return salario + bono;
-  }
-};
-
-class Desarrollador : public Empleado {
-private:
-  int horasExtra;
-  float pagoHoraExtra;
-
-public:
-  Desarrollador(string nombre, float salario, fecha fechaContratacion, int horasExtra, float pagoHoraExtra) : Empleado(nombre, salario, fechaContratacion) {
-    this->horasExtra = horasExtra;
-    this->pagoHoraExtra = pagoHoraExtra;
-  }
-
-  float calcularSalario() { // Método virtual
-    return salario + (horasExtra * pagoHoraExtra);
-  }
-};
-
-class Diseñador : public Empleado {
-private:
-  float comisionProyectos;
-
-public:
-  Diseñador(string nombre, float salario, fecha fechaContratacion, float comisionProyectos) : Empleado(nombre, salario, fechaContratacion) {
-    this->comisionProyectos = comisionProyectos;
-  }
-
-  float calcularSalario() { // Método virtual
-    return salario + comisionProyectos;
-  }
-};
-
-int main() {
-  // Creando empleados de diferentes tipos
-  Empleado PedroPaz("Pedro Paz", 2500, {2022, 3, 1});
-  Gerente LiamSol("Liam Sol", 2800, {2021, 7, 15}, 400);
-  Desarrollador MariaVelez("Maria Velez", 2000, {2023, 5, 10}, 10, 35);
-  Diseñador SimeoVill("Simeo Vill", 1800, {2024, 2, 15}, 200);
-
-  // Demostrando el polimorfismo para calcular salarios
-  cout << "Nombre: " << PedroPaz.getNombre() << endl;
-  cout << "Salario: " << PedroPaz.calcularSalario() << endl;
-
-  cout << "Nombre: " << LiamSol.getNombre() << endl;
-  cout << "Salario: " << LiamSol.calcularSalario() << endl;
-
-  cout << "Nombre: " << MariaVelez.getNombre() << endl;
-  cout << "Salario: " << MariaVelez.calcularSalario() << endl;
-
-  cout << "Nombre: " << SimeoVill.getNombre() << endl;
-  cout << "Salario: " << SimeoVill.calcularSalario() << endl;
-
-  return 0;
-}
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#include <iostream>
 #include <string>
 using namespace std;
 
@@ -190,7 +50,8 @@ class Gerente : public Empleado {
 private:
     float bono;
 public:
-    Gerente(string nombre, float salario, int anio, int mes, int dia, float bono) : Empleado(nombre, salario, anio, mes, dia) {
+    Gerente(string nombre, float salario, int anio, int mes, int dia, float bono)
+    : Empleado(nombre, salario, anio, mes, dia) {
         this->bono = bono;
     }
     float calcularSalario() override {
@@ -202,7 +63,8 @@ private:
     int horasExtra;
     float pagoHoraExtra;
 public:
-    Desarrollador(string nombre, float salario, int anio, int mes, int dia, int horasExtra, float pagoHoraExtra) : Empleado(nombre, salario, anio, mes, dia) {
+    Desarrollador(string nombre, float salario, int anio, int mes, int dia, int horasExtra, float pagoHoraExtra)
+    : Empleado(nombre, salario, anio, mes, dia) {
         this->horasExtra = horasExtra;
         this->pagoHoraExtra = pagoHoraExtra;
     }
@@ -215,10 +77,33 @@ class Diseñador : public Empleado {
 private:
     float comisionProyectos;
 public:
-    Diseñador(string nombre, float salario, int anio, int mes, int dia, float comisionProyectos) : Empleado(nombre, salario, anio, mes, dia) {
+    Diseñador(string nombre, float salario, int anio, int mes, int dia, float comisionProyectos)
+    : Empleado(nombre, salario, anio, mes, dia) {
         this->comisionProyectos = comisionProyectos;
     }
     float calcularSalario() override {
         return getSalario() + comisionProyectos;
     }
 };
+
+int main() {
+    // Creando empleados de diferentes tipos
+    int cant = 3;
+    Empleado* arreglo[cant];
+    arreglo[0] = new Gerente("Pedro Paz", 2500, 2022, 3, 1, 400);
+    arreglo[1] = new Desarrollador("Maria Velez", 2000, 2023, 5, 10, 10, 35);
+    arreglo[2] = new Diseñador("Simeo Vill", 1800, 2024, 2, 15, 200);
+   
+    // Demostrando el polimorfismo para calcular salarios
+    for(int i = 0; i < cant; i++){
+        cout << "Nombre: " << arreglo[i]->getNombre() << endl;
+        cout << "Salario: " << arreglo[i]->calcularSalario() << endl;
+    }
+   
+    // Liberar memoria
+    for(int i = 0; i < cant; i++){
+        delete arreglo[i];
+    }
+   
+    return 0;
+}
