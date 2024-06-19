b8d0718f200745725b270a77525548482b55f4ab
keduard2006 <131702439+keduard2006@users.noreply.github.com>
2024-06-18 20:22:53 -0500

    Update Sistema de Gestion de Recursos Humanos.cpp

--- Sistema de Gestion de Recursos Humanos.cpp
+++ Sistema de Gestion de Recursos Humanos.cpp
@@ -110,6 +110,7 @@ int main() {
 ____________________________________________________________________________________________________________________________________________
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Empleado {
@@ -127,7 +128,7 @@ public:
         this->fechaContratacionMes = mes;
         this->fechaContratacionDia = dia;
     }
-    virtual float calcularSalario() = 0; // Método virtual puro
+    virtual float calcularSalario() ; // Método virtual puro
     string getNombre() const {
         return nombre;
     }
@@ -230,7 +231,6 @@ public:
         for (int i = 0; i < cantidad; ++i) {
             if (empleados[i] == empleado) {
                 delete empleados[i];
-                // Mover los elementos hacia la izquierda para llenar el espacio vacío
                 for (int j = i; j < cantidad - 1; ++j) {
                     empleados[j] = empleados[j + 1];
                 }
@@ -264,17 +264,30 @@ public:
 int main() {
     // Creando empleados de diferentes tipos
     int cant = 3;
-    Empleado* arreglo[cant];
-    arreglo[0] = new Gerente("Pedro Paz", 2500, 2022, 3, 1, 400);
-    arreglo[1] = new Desarrollador("Maria Velez", 2000, 2023, 5, 10, 10, 35);
-    arreglo[2] = new Diseñador("Simeo Vill", 1800, 2024, 2, 15, 200);
+    Departamento<Empleado>* arreglo[cant];
+    Departamento<Empleado>* departamentoGerentes =new Departamento<Gerente>;
+    arreglo[0] = departamentoGerentes;
+    departamentoGerentes->agregarEmpleado(new Gerente("Pedro Paz", 2500, 2022, 3, 1, 400));
+    Departamento<Desarrollador> departamentoDesarrolladores;
+    arreglo[1] = &departamentoDesarrolladores;
+    departamentoDesarrolladores.agregarEmpleado(new Desarrollador("Maria Velez", 2000, 2023, 5, 10, 10, 35));
+    arreglo[2] = Departamento<Disenador> departamentoDisenadores;
+    departamentoDisenadores.agregarEmpleado(new Diseñador("Simeo Vill", 1800, 2024, 2, 15, 200));
    
     // Demostrando el polimorfismo para calcular salarios
     for(int i = 0; i < cant; i++){
         cout << "Nombre: " << arreglo[i]->getNombre() << endl;
         cout << "Salario: " << arreglo[i]->calcularSalario() << endl;
     }
-   
+    
+    //listar empleados de cadad departamento
+    cout << "Departamento de Gerentes:" << endl;
+    departamentoGerentes.listarEmpleados();
+    cout << "Departamento de Desarrolladores:" << endl;
+    departamentoDesarrolladores.listarEmpleados();
+    cout << "Departamento de Diseñadores:" << endl;
+    departamentoDisenadores.listarEmpleados();
+
     // Liberar memoria
     for(int i = 0; i < cant; i++){
         delete arreglo[i];
